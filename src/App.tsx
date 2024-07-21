1f01d32e80f145812317fa11a2a478d6652e80f2
ilzat <ilzat.sadykov.2006@bk.ru>
2024-07-21 13:37:58 +0600

    'сделал компонент Header.tsx скачал дополнения для mui'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import './App.css';
+import Header from './Components/Header/Header.tsx';
+import {Routes} from 'react-router-dom';
 
 const App = () => (
-    <>
+  <>
+    <Header/>
+    <Routes>
 
-    </>
+    </Routes>
+  </>
 );
 
-export default App
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import {NavLink} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component={NavLink} sx={{flexGrow: 1}} to="/"
+                      className="text-white text-decoration-none">
+            Contacts
+          </Typography>
+          <Button color="inherit">add new Contact</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
